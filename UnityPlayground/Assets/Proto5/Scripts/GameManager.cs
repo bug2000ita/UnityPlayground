bc2893fd54d183e49e498d32ad72dd20c2c25f91
Giovanni Mariotta <giovanni.mariotta@thermofisher.com>
2021-02-07 21:59:15 +0100

    Proto5 added

---
+++ UnityPlayground/Assets/Proto5/Scripts/GameManager.cs
@@ -0,0 +1,39 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    public GameObject[] items;
+    private float delay = 1;
+    private float repeatSpawn = 1;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        StartCoroutine(SpawnTargets());
+        //InvokeRepeating("SpawnTarget", delay, repeat);
+    }
+
+    IEnumerator SpawnTargets()
+    {
+        while (true)
+        {
+            yield return new WaitForSeconds(repeatSpawn);
+            SpawnTarget();
+        }
+            
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void SpawnTarget()
+    {
+        int randomIndex = Random.Range(0, 4);
+        Instantiate(items[randomIndex]);
+    }
+}
---
+++ UnityPlayground/Assets/Proto5/Scripts/TargetScript.cs
@@ -0,0 +1,49 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TargetScript : MonoBehaviour
+{
+    private Rigidbody objectRB;
+
+    private float maxSpeed = 14f;
+    private float minspeed = 10f;
+    private float maxTorch = 10f;
+    private float xMaxPosition = 4;
+    private float yPosition = 0;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        objectRB = GetComponent<Rigidbody>();
+
+        objectRB.AddForce(RandomForce(), ForceMode.Impulse);
+        objectRB.AddTorque(RandomTorch(),ForceMode.Impulse);
+
+        transform.position = RandomSpawnPos();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(transform.position.y<-10)
+        {
+            Destroy(gameObject);
+        }
+    }
+
+    Vector3 RandomForce()
+    {
+        return Vector3.up * Random.Range(minspeed, maxSpeed);
+    }
+
+    Vector3 RandomTorch()
+    {
+        return new Vector3(Random.Range(-maxTorch, maxTorch), Random.Range(-maxTorch, maxTorch), Random.Range(-maxTorch, maxTorch));
+    }
+
+    Vector3 RandomSpawnPos()
+    {
+        return new Vector3(Random.Range(-xMaxPosition, xMaxPosition), -yPosition,0);
+    }
+}
