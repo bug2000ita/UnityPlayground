e89abbbaaf7bbdb43c9a0247dad4426279dd1885
Giovanni Mariotta <giovanni.mariotta@thermofisher.com>
2021-02-08 23:32:57 +0100

    Challenge 5 completed

---
+++ UnityPlayground/Assets/Challenge 5/Scripts/DestroyObjectX.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DestroyObjectX : MonoBehaviour
+{
+    void Start()
+    {
+        
+    }
+
+    private void OnMouseDown()
+    {
+        Destroy(gameObject); 
+    }
+
+
+}
